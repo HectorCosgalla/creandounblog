8a259b35b10fb6d7288d83d037c601de2b048e16
Hector Cosgalla <valcos98@hotmail.com>
2023-01-09 20:36:31 -0600

    maybe what i need itx2

--- src/main/java/com/valcos98/controller/HomeController.java
+++ src/main/java/com/valcos98/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.valcos98.creandounblog.controller;
+package com.valcos98.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/valcos98/creandounblogApplication.java
+++ src/main/java/com/valcos98/creandounblogApplication.java
@@ -1,13 +1,13 @@
-package com.valcos98.creandounblog;
+package com.valcos98;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CreandounblogApplication {
+public class creandounblogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CreandounblogApplication.class, args);
+		SpringApplication.run(creandounblogApplication.class, args);
 	}
 
 }
