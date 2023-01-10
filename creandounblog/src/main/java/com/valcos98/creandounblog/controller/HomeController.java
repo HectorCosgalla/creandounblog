430cf7de35c3e7dcc7a6c0a9366f707f7375307e
Hector Cosgalla <valcos98@hotmail.com>
2023-01-09 19:23:29 -0600

    page RUNNING FOR THE 2ND TIMEgit add .

---
+++ creandounblog/src/main/java/com/valcos98/creandounblog/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.valcos98.creandounblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String minimain(Model model){
+        return "index";
+    }
+
+    @GetMapping("/posts/primer_post")
+    public String primerPost(Model model){
+        return "/posts/primer_post";
+    }
+    
+}
