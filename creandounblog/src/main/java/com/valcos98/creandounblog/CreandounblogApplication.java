ebdbf01ff27bfeee803492be3eca8f8a0bf3fe40
Hector Cosgalla <valcos98@hotmail.com>
2023-01-09 18:42:57 -0600

    adding spring boot and moving everything i had to templates

---
+++ creandounblog/src/main/java/com/valcos98/creandounblog/CreandounblogApplication.java
@@ -0,0 +1,13 @@
+package com.valcos98.creandounblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreandounblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreandounblogApplication.class, args);
+	}
+
+}
