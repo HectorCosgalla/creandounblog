6d71e05cab8364370dc911bd750f415158bbc340
Hector Cosgalla <valcos98@hotmail.com>
2023-04-21 14:49:44 -0600

    A brand new blog

---
+++ src/main/java/com/valcos98/creandounblog/CreandoUnBlogApplication.java
@@ -0,0 +1,13 @@
+package com.valcos98.creandounblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreandoUnBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreandoUnBlogApplication.class, args);
+	}
+
+}
